f4e93d05277783ce93d417cd3f5694df55870b9c
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 05:47:23 +0800

    Update _flaming9.js

--- plugins/_flaming9.js
+++ plugins/_flaming9.js
@@ -1,10 +1,10 @@
 var fetch = require('node-fetch')
 var handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .naruto BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .naruto CAHX'
   m.reply('_Proses..._')
   var res = `https://api.botcahx.biz.id/api/photooxy/naruto?text=${response[0]}&apikey=Admin`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© JarspBot`, m, false)
 }
 handler.help = ['naruto'].map(v => v + ' <text>')
 handler.tags = ['maker']
