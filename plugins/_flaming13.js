20e71a2bd2a38d1fa323184e11906474ba6781bd
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 05:42:47 +0800

    Update _flaming13.js

--- plugins/_flaming13.js
+++ plugins/_flaming13.js
@@ -1,10 +1,10 @@
 var fetch = require('node-fetch')
 var handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .pictlove BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .pictlove CAHX'
   m.reply('_Proses..._')
   let res = `https://api.botcahx.biz.id/api/photooxy/picture-of-love?text=${response[0]}&apikey=Admin`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© JarspBot`, m, false)
 }
 handler.help = ['pictlove'].map(v => v + ' <text>')
 handler.tags = ['photooxy']
