b0bab03ec29ddf4cbcf003c607c435779def9324
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 05:35:59 +0800

    Update _autoresponlink.js

--- plugins/_autoresponlink.js
+++ plugins/_autoresponlink.js
@@ -4,9 +4,11 @@ let handler = m => m
 handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `Invite Group
-• 15 Day / Rp 5k
-• 30 Day / Rp 10k
+    let teks = `Invite Bot to Group
+• 7 Day / Rp 5k
+• 14 Day / Rp 10k
+• 21 Day / Rp 15k
+• 30 Day / Rp 20k
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
