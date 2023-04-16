4cc7391ea0a8e7d289b50cddffe1c010ca4a8993
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 05:44:07 +0800

    Update _flaming16.js

--- plugins/_flaming16.js
+++ plugins/_flaming16.js
@@ -2,10 +2,10 @@
 var fetch = require('node-fetch')
 var handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .cwood BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .cwood CAHX'
   m.reply('_Proses..._')
   var res = `https://api.botcahx.biz.id/api/photooxy/carved-wood?text=${response[0]}&apikey=Admin`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© JarspBot`, m, false)
 }
 handler.help = ['cwood'].map(v => v + ' <text>')
 handler.tags = ['photooxy']
