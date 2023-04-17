149b78c33189f8336e09a274e73d91da29054c4d
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 19:46:49 +0800

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${ye} Bot Ini Menggunakan Script Recode:\n• https://github.com/BOTCAHX/RTXZY-MD\n\nOriginal Base :\n• https://github.com/HelgaIlham/ZukaBet\n\nMau Script Recodean Gw? Chat Owner
 `
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
