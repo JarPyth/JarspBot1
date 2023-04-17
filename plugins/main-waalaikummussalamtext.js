6af7b0cab2a4a19a7b0a8667fb6c979444a48902
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 19:11:54 +0800

    Update main-waalaikummussalamtext.js

--- plugins/main-waalaikummussalamtext.js
+++ plugins/main-waalaikummussalamtext.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hi ${ye} Ada Yang Bisa Bot Bantu?\n\nKetik *.menu atau /menu, #menu* Untuk Melihat Menu Bot Nya :)\n\n> *Contoh : .menu*
+Hi ${ye} Ada Yang Bisa Jarsp Bantu?\n\nKetik *.menu atau /menu, #menu* Untuk Melihat Menu Bot Nya\n\n> *Contoh : .menu*
 `
 let footer= 'Saya Adalah Bot WhatsApp Multi Device Yang Diciptakan Oleh Jarspy.\n\nGunakanlah Bot Ini Sebijak Mungkin Demi kenyamanan Bersama\n\nHarap Untuk Tidak Menelpon, Meminta Save, Atau Spam Dalam Penggunaan Bot\n\nJika Ada Kendala Hubungi Owner / Creator'
 conn.send2But(m.chat, esce, footer, 'Menu', '.menu', 'Thanks', 'iya', m,)
