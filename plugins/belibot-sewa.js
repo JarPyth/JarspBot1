7b7df0716cfdae35e735b036a55a3c561386e848
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-04-17 17:15:40 +0800

    Update belibot-sewa.js

--- plugins/belibot-sewa.js
+++ plugins/belibot-sewa.js
@@ -5,11 +5,11 @@ var handler = async (m, {
  usedPrefix,
  command
  }) => {
-var gh = 'https://github.com/BOTCAHX'
+var gh = 'https://github.com/JarPyth'
   let buttonMessage= {
 'document':{'url':gh},
 'mimetype':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-'fileName':'Payment Methode',
+'fileName':'Payment Method',
 'fileLength':'99999999999999',
 'pageCount':'999',
 'contextInfo':{
@@ -18,12 +18,12 @@ var gh = 'https://github.com/BOTCAHX'
 'mediaUrl':gh,
 'mediaType':2,
 'previewType':'pdf',
-'title':'Follow Github Gua',
-'body':'© BOTCAHX',
-'thumbnail':await(await fetch('https://telegra.ph/file/f4063ab3071f5c4af765a.png')).buffer(),
+'title':'Follow My Github',
+'body':'© JarspBot',
+'thumbnail':await(await fetch('https://telegra.ph/file/47020046e1e3e365a3764.jpg')).buffer(),
 'sourceUrl':gh}},
-'caption':'Hi, Silahkan pilih opsi di bawah ini.\n*Payment*\n\n*x* Dana: 082221792667\n*x* Pulsa: 082221792667',
-'footer':'©TioXd',
+'caption':'Hi, Silahkan pilih opsi di bawah ini.\n*Payment*\n\n*x* Dana/Gopay: 082148864989\n*x* Pulsa: 082148864989',
+'footer':'© Jarspy',
 'buttons':[
 {'buttonId':'.menu','buttonText':{'displayText':'ᴍᴇɴᴜ'},'type':1},
 {'buttonId':'.donasi','buttonText':{'displayText':'ᴅᴏɴᴀsɪ'},'type':1}
